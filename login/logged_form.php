ee89d1228bbe9686fede18157a23e633c6dff2cb
Carlos Perez Priego <carlos.perez@ecologikal.net>
2011-10-04 14:51:44 -0500

    Logged Form

--- login/logged_form.php
+++ login/logged_form.php
@@ -1,17 +1,17 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT']."/github/Eckl/_config/bootstrap.php");
-	$tracking_settings=members_get_info("settings_tracking");
-	include_once(_JS_URL_."login/logged_form.php");
-?>
-<div>
-    <?php if($tracking_settings==0 && $GEN_USUARIO){?>
-    <div id="tracking_settings" title="Tracking Setting	">
-		<div id="validateTips_login" class="validateTips">&nbsp;</div>
-            <label for="login_form_user"><?php echo LANGUAGE_MEMBERS_TEXT_USER;?></label>
-            <input type="text" name="login_form_user" id="login_form_user"  class="text ui-widget-content ui-corner-all" />
-            <label for="login_form_password"><?php echo LANGUAGE_MEMBERS_TEXT_PASSWORD;?></label>
-            <input type="password" name="login_form_password" id="login_form_password" class="text ui-widget-content ui-corner-all" />
-            <label for="login_form_remember_session"><input name="login_form_remember_session" type="checkbox" id="login_form_remember_session" value="1" />
-            No cerrar sesi&oacute;n</label>
-    </div>
-    <?php } ?>
-</div>
+-<?php require_once($_SERVER['DOCUMENT_ROOT']."/github/Eckl/_config/bootstrap.php");
+-    $tracking_settings=members_get_info("settings_tracking");
+-    include_once(_JS_URL_."login/logged_form.php");
+-?>
+-<div>
+-    <?php if($tracking_settings==0 && $GEN_USUARIO){?>
+-    <div id="tracking_settings" title="Tracking Setting    ">
+-        <div id="validateTips_login" class="validateTips">&nbsp;</div>
+-            <label for="login_form_user"><?php echo LANGUAGE_MEMBERS_TEXT_USER;?></label>
+-            <input type="text" name="login_form_user" id="login_form_user"  class="text ui-widget-content ui-corner-all" />
+-            <label for="login_form_password"><?php echo LANGUAGE_MEMBERS_TEXT_PASSWORD;?></label>
+-            <input type="password" name="login_form_password" id="login_form_password" class="text ui-widget-content ui-corner-all" />
+-            <label for="login_form_remember_session"><input name="login_form_remember_session" type="checkbox" id="login_form_remember_session" value="1" />
+-            No cerrar sesi&oacute;n</label>
+-    </div>
+-    <?php } ?>
+-</div>
